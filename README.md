6ae0bfb03ffb1dd7d6eb269451958bd27ca456cd
Wathsara Wishwantha Daluwatta <31571237+Wathsara@users.noreply.github.com>
2021-01-15 01:37:43 +0530

    Updated the app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# api" 
+"# api"
+
+Before Run Install
+
+pip3 install flask
+pip3 install flask_cors
+pip3 install PyMySQL
+pip3 install Flask-MySQL
+pip3 install Flask-JWT 
--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={r"/": {"origins": "*"}})
-app.config['SECRET_KEY'] = "iCS_2021_Algeria{__NeVer__dO__tHaT__to__YoUr__SecreTS__}"
+app.config['SECRET_KEY'] = "<Your Private Key >"
